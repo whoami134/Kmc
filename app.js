9a81ae49d7004672e38ab2255fd504ccf50d62d1
leo duggirala <159580147+whoami134@users.noreply.github.com>
2024-07-25 15:36:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import BookSessionScreen from './src/screens/BookSessionScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="BookSession" component={BookSessionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
